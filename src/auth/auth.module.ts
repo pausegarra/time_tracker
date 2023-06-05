d61571b031a9bb6891df5d3a5e3675a54b6c5ed7
psegarra <pausegarra.b@gmail.com>
2023-06-05 12:42:23 +0200

    ✨ implement jwt & passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,18 @@ import { AuthService } from './application/auth.service';
 import { UserModule } from 'src/user/user.module';
 import { RegisterController } from './infrastructure/controllers/register.ctrl';
 import { UserService } from 'src/user/application/user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './infrastructure/strategies/jwt.strategy';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: 'random',
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   controllers: [LoginController, RegisterController],
-  providers: [AuthService, UserService],
+  providers: [AuthService, UserService, JwtStrategy],
 })
 export class AuthModule {}
