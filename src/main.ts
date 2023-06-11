2ce0883404b2dd67b39541fcacd1300e34ad87ac
psegarra <pausegarra.b@gmail.com>
2023-06-11 21:42:39 +0200

    ♻️ modify credentials and origin in cors config

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:5173',
+    credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
