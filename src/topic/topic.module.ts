3873518592d0a96d9cdd8511f8eb2294554ef4f9
psegarra <pausegarra.b@gmail.com>
2023-06-18 19:06:20 +0200

    ♻️ refactor topic services

--- src/topic/topic.module.ts
+++ src/topic/topic.module.ts
@@ -3,11 +3,8 @@ import { CreateTopicController } from './infrastructure/controllers/create-topic
 import { MysqlRepository } from './infrastructure/repositories/mysql.repository';
 import { TopicModel } from './infrastructure/model/topic.model';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { CreateTopicService } from './services/create-topic.service';
 import { GetTopicsController } from './infrastructure/controllers/get-topics.ctrl';
-import { GetTopicService } from './services/get-topics.service';
 import { DeleteTopicController } from './infrastructure/controllers/delete-topic.ctrl';
-import { DeleteTopicService } from './services/delete-topic.service';
 import { TopicService } from './services/topic.service';
 
 @Module({
@@ -17,9 +14,6 @@ import { TopicService } from './services/topic.service';
       provide: 'TopicRepository',
       useClass: MysqlRepository,
     },
-    CreateTopicService,
-    GetTopicService,
-    DeleteTopicService,
     TopicService,
   ],
   controllers: [
