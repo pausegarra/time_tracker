e1195334e85408e11760359816eed8e09cc9800a
psegarra <pausegarra.b@gmail.com>
2023-11-07 19:13:03 +0100

    🐛 refres user data on logout and ci/cd with github workflow

--- client/src/auth/hooks/use-logout.hook.js
+++ client/src/auth/hooks/use-logout.hook.js
@@ -1,13 +1,17 @@
+import React from 'react'
 import { logoutService as _logoutService } from '../services/logout.service'
 import { notificationsService as _notificationsService } from '../../notifications/services/notifications.service'
 import { useNavigate } from 'react-router-dom'
+import {AppContext} from '@/shared/contexts/app.context'
 
 export function useLogout (logoutService = _logoutService, notificationsService = _notificationsService) {
   const navigate = useNavigate()
+  const {refresh} = React.useContext(AppContext)
 
   async function handleLogout () {
     try {
       await logoutService.logout()
+      await refresh()
       navigate('/')
     } catch (err) {
       notificationsService.unhandledError()
